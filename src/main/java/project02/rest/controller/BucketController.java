e06db9839373d6d8fcc3e849fbbda0cc7f8f7231
Yotsawat <yotsawat_l@cmu.ac.th>
2023-10-22 19:39:11 +0700

    Bucket

---
+++ src/main/java/project02/rest/controller/BucketController.java
@@ -0,0 +1,24 @@
+package project02.rest.controller;
+
+import jakarta.servlet.ServletException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+import project02.rest.util.CloudStorageHelper;
+
+import java.io.IOException;
+
+@Controller
+@RequiredArgsConstructor
+public class BucketController {
+    final CloudStorageHelper cloudStorageHelper;
+    @PostMapping("/uploadFile")
+    public ResponseEntity<?> uploadFile(@RequestPart(value = "file")MultipartFile file)
+        throws IOException, ServletException {
+        return
+                ResponseEntity.ok(this.cloudStorageHelper.getImageUrl(file,"imageupload-e5081.appspot.com"));
+    }
+}
