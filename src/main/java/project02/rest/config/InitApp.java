a4e67fad827515811bf95bbd73308ecaccdd465d
Yotsawat <yotsawat_l@cmu.ac.th>
2023-10-23 16:23:35 +0700

    UpImg to db

--- src/main/java/project02/rest/config/InitApp.java
+++ src/main/java/project02/rest/config/InitApp.java
@@ -17,10 +17,7 @@ import project02.rest.security.user.Role;
 import project02.rest.security.user.User;
 import project02.rest.security.user.UserRepository;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.Date;
+
 import java.util.List;
 
 @Component
@@ -49,13 +46,13 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
 
 
         User userT1 = new User();
-        userT1.setUsername("floky");
-        userT1.setFirstname("floky");
+        userT1.setUsername("broky");
+        userT1.setFirstname("broky");
         userT1.setLastname("faze");
         userT1.setPassword("password");
-        userT1.setEmail("floky@email.com");
+        userT1.setEmail("broky@email.com");
         userT1.setRoles(List.of(Role.ROLE_TEACHER));
-        userT1.setProfileImg("");
+        userT1.setProfileImg("https://storage.googleapis.com/download/storage/v1/b/imageupload-e5081.appspot.com/o/2023-10-23%20160516296-borky.jpg?generation=1698051916574394&alt=media");
         userRepository.save(userT1);
 
 
@@ -66,7 +63,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
         userT2.setPassword("simpleed");
         userT2.setEmail("simple@navi.com");
         userT2.setRoles(List.of(Role.ROLE_TEACHER));
-        userT2.setProfileImg("");
+        userT2.setProfileImg("https://storage.googleapis.com/download/storage/v1/b/imageupload-e5081.appspot.com/o/2023-10-23%20160516068-Simple.png?generation=1698051916410803&alt=media");
         userRepository.save(userT2);
 
         User userS1 = new User();
@@ -76,7 +73,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
         userS1.setEmail("niko@gtwo.com");
         userS1.setPassword("nikogod");
         userS1.setRoles(List.of(Role.ROLE_STUDENT));
-        userS1.setProfileImg("");
+        userS1.setProfileImg("https://storage.googleapis.com/download/storage/v1/b/imageupload-e5081.appspot.com/o/2023-10-23%20160515877-Niko.png?generation=1698051916174669&alt=media");
         userRepository.save(userS1);
 
         User userS2 = new User();
@@ -86,7 +83,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
         userS2.setPassword("qqgod");
         userS2.setEmail("qq@god.com");
         userS2.setRoles(List.of(Role.ROLE_STUDENT));
-        userS2.setProfileImg("");
+        userS2.setProfileImg("https://storage.googleapis.com/download/storage/v1/b/imageupload-e5081.appspot.com/o/2023-10-23%20160515424-qqGOD.png?generation=1698051915984598&alt=media");
         userRepository.save(userS2);
 
         User userS3 = new User();
@@ -96,7 +93,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
         userS3.setPassword("cigmith");
         userS3.setEmail("cig@god.com");
         userS3.setRoles(List.of(Role.ROLE_STUDENT));
-        userS3.setProfileImg("");
+        userS3.setProfileImg("https://storage.googleapis.com/download/storage/v1/b/imageupload-e5081.appspot.com/o/2023-10-23%20160514931-CigaretteS.jpg?generation=1698051915464741&alt=media");
         userRepository.save(userS3);
 
         User userS4 = new User();
@@ -106,7 +103,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
         userS4.setPassword("ogCEO");
         userS4.setEmail("notail@og.com");
         userS4.setRoles(List.of(Role.ROLE_STUDENT));
-        userS4.setProfileImg("");
+        userS4.setProfileImg("https://storage.googleapis.com/download/storage/v1/b/imageupload-e5081.appspot.com/o/2023-10-23%20160514648-Notail.jpg?generation=1698051914985930&alt=media");
         userRepository.save(userS4);
 
         User userS5 = new User();
@@ -116,7 +113,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
         userS5.setPassword("dogson");
         userS5.setEmail("topson@trunda.com");
         userS5.setRoles(List.of(Role.ROLE_STUDENT));
-        userS5.setProfileImg("");
+        userS5.setProfileImg("https://storage.googleapis.com/download/storage/v1/b/imageupload-e5081.appspot.com/o/2023-10-23%20160514250-Topson.jpeg?generation=1698051914721319&alt=media");
         userRepository.save(userS5);
 
         User userS6 = new User();
@@ -126,7 +123,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
         userS6.setPassword("carrygod");
         userS6.setEmail("ana@humlek.com");
         userS6.setRoles(List.of(Role.ROLE_STUDENT));
-        userS6.setProfileImg("");
+        userS6.setProfileImg("https://storage.googleapis.com/download/storage/v1/b/imageupload-e5081.appspot.com/o/2023-10-23%20160514086-Ana.jpg?generation=1698051914357913&alt=media");
         userRepository.save(userS6);
 
         User userS7 = new User();
@@ -136,7 +133,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
         userS7.setPassword("RakPgra");
         userS7.setEmail("jason@susanto.com");
         userS7.setRoles(List.of(Role.ROLE_STUDENT));
-        userS7.setProfileImg("");
+        userS7.setProfileImg("https://storage.googleapis.com/download/storage/v1/b/imageupload-e5081.appspot.com/o/2023-10-23%20160513831-Jason.png?generation=1698051914185777&alt=media");
         userRepository.save(userS7);
 
         User userS8 = new User();
@@ -146,7 +143,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
         userS8.setPassword("hrkzaza");
         userS8.setEmail("hrk@taupom.com");
         userS8.setRoles(List.of(Role.ROLE_STUDENT));
-        userS8.setProfileImg("");
+        userS8.setProfileImg("https://storage.googleapis.com/download/storage/v1/b/imageupload-e5081.appspot.com/o/2023-10-23%20160513625-hrk.jpg?generation=1698051913921112&alt=media");
         userRepository.save(userS8);
 
         User userS9 = new User();
@@ -166,7 +163,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
         userS10.setPassword("lucky888");
         userS10.setEmail("lotto@gg.com");
         userS10.setRoles(List.of(Role.ROLE_STUDENT));
-        userS10.setProfileImg("");
+        userS10.setProfileImg("https://storage.googleapis.com/download/storage/v1/b/imageupload-e5081.appspot.com/o/2023-10-23%20160512500-aek.jpg?generation=1698051913669997&alt=media");
         userRepository.save(userS10);
 
 
@@ -176,6 +173,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
 
         Teacher t2 = new Teacher();
         t2.setUser(userT2);
+
         teacherRepository.save(t2);
 
 
